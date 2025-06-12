7216048209fb3b99023233fe3c59f88cea66554e
Anonymous <cloxley4@gmail.com>
2025-06-12 16:54:36 -0400

    Collision Broken

---
+++ config.py
@@ -0,0 +1,35 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+
+PLAYER_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+
+TILESIZE = 32
+
+# COLORS
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+FPS = 60
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B.........P.........B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBB...BBBBBBBBB',
+    'B..................B',
+    'BBBB.....T......BBBB',
+    'B..................B',
+    'BBBBBBBB...BBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B.......BBB........B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
